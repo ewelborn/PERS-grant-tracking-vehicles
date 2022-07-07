dbc39631ed326db9100db7a1eff9a1775c69ce99
Ethan Welborn <77039258+ewelborn@users.noreply.github.com>
2022-07-07 12:20:19 -0500

    MultiTracker added, additional changes made
    Optical flow tracking was removed in favor of OpenCV's legacy MultiTracker system. The user no longer has to specify real-time delays for detection, rather, the user specifies how many frames the script should delay before detecting again (I have no clue why I didn't implement it this way before).

--- vehicleTrackingWithHomographyAndOpticalFlow.py
+++ vehicleTrackingWithHomographyAndOpticalFlow.py
@@ -32,7 +32,7 @@ SAVE_RESULT_AS_VIDEO = False
 OVERWRITE_PREVIOUS_RESULT = False
 
 # Video file to write to (no effect if SAVE_RESULT_AS_VIDEO = False)
-OUTPUT_VIDEO_PATH = "output.mp4"
+OUTPUT_VIDEO_PATH = "multiTracker2.mp4"
 
 # What detection model are we using? (YOLO, MASKRCNN)
 DETECTION_MODEL = "MASKRCNN"
@@ -44,8 +44,8 @@ COCO_LABELS_TO_DETECT = ["car", "truck"]
 # How confident should our detection model be when detecting cars? (0 is no confidence, 1 is completely confident)
 DETECTION_MINIMUM_CONFIDENCE = 0.3
 
-# How many seconds should we wait until the detection model is allowed to detect cars again?
-DETECTION_REFRESH_RATE = 2
+# How many frames should we wait until the detection model is allowed to detect cars again?
+DETECTION_REFRESH_RATE = 10
 
 # Where are the YOLOv4 configuration and weight files contained? (No effect if DETECTION_MODEL != "YOLO")
 YOLO_CONFIG_PATH = "yolov4.cfg"
@@ -96,12 +96,18 @@ DRAW_BOUNDING_BOXES = True
 DRAWING_THICKNESS = 3
 
 # Should the optical flow points be drawn on the video?
-DRAW_OPTICAL_FLOW_POINTS = True
+DRAW_OPTICAL_FLOW_POINTS = False
 
 # How large should the optical flow points look like when drawn (in pixels)? (No effect if DRAW_OPTICAL_FLOW_POINTS = False)
 OPTICAL_FLOW_POINT_DRAWING_SIZE = 5
 
 ### END OF CONFIG
+
+# Ensure we're using the proper OpenCV package
+if hasattr(cv2, "legacy") == False:
+    print("cv2.legacy is missing! Please install (or enable) an OpenCV version that has this package.")
+    print("The legacy package is required so that the multitracker object can be used.")
+    print("Recommended pip package is 'opencv-contrib-python' (4.6.0.66)")
 
 # Make sure the detection model is valid
 if not(DETECTION_MODEL in ["YOLO","MASKRCNN"]):
@@ -182,6 +188,9 @@ class BoundingBox():
     def getEndY(self):
         return self.y + self.height
 
+    def getArea(self):
+        return self.width * self.height
+
     def getOverlap(bb1,bb2):
         # https://stackoverflow.com/questions/25349178/calculating-percentage-of-bounding-box-overlap-for-image-detector-evaluation
         x = max(bb1.getX(), bb2.getX())
@@ -193,10 +202,8 @@ class BoundingBox():
             return 0.0
 
         overlapArea = (endX - x) * (endY - y)
-        bb1Area = bb1.getWidth() * bb1.getHeight()
-        bb2Area = bb2.getWidth() * bb2.getHeight()
 
-        return overlapArea / float(bb1Area + bb2Area - overlapArea)
+        return overlapArea / float(bb1.getArea() + bb2.getArea() - overlapArea)
 
 class Car():
     def __init__(self,ID):
@@ -222,19 +229,18 @@ manualHomography_pixelsToMeters = 0
 # Only used for drawing masks from Mask-RCNN
 maskRCNNDrawingMask = None
 
+multiTracker = cv2.legacy.MultiTracker_create()
+
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 out = None
 
 # Keep reading frames from the video until it's over
-currentTime = time.time()
-endTime = time.time()
 progress = 0
 firstFrame = True
 lastGrayFrame = None
 totalFrames = 0
 while True:
-    currentTime = time.time()
-    progress += currentTime - endTime
+    progress += 1
     ret, frame = cap.read()
 
     totalFrames += 1
@@ -320,7 +326,6 @@ while True:
 
     if firstFrame:
         # Make sure the timer is accurate and ensure that detection is done on the very first frame
-        currentTime = time.time()
         progress = DETECTION_REFRESH_RATE
 
     firstFrame = False
@@ -335,6 +340,9 @@ while True:
         # Clear the cars list and prepare it for the new cars
         oldCars = currentCars
         currentCars = []
+
+        # Clear the multitracker as well
+        multiTracker = cv2.legacy.MultiTracker_create()
 
         if DETECTION_MODEL == "YOLO":
             imageBlob = cv2.dnn.blobFromImage(frame, 0.003922, (416,416), swapRB = True, crop = False)
@@ -427,7 +435,7 @@ while True:
 
                 if predictionConfidence >= DETECTION_MINIMUM_CONFIDENCE and predictedClassLabel in COCO_LABELS_TO_DETECT:
                     car = Car(currentCarID)
-                    (x, y, endX, endY) = (boundingBoxes[0, 0, i, 3:7] * np.array([frameWidth, frameHeight, frameWidth, frameHeight])).astype("int")
+                    x, y, endX, endY = (boundingBoxes[0, 0, i, 3:7] * np.array([frameWidth, frameHeight, frameWidth, frameHeight])).astype("int")
                     car.boundingBox = BoundingBox(x, y, endX - x, endY - y)
 
                     # Determine if this car has already been detected, if so, then reuse the old ID
@@ -469,10 +477,10 @@ while True:
                     #cv2.waitKey(2000)
 
                     # Find corners on the car that are good for tracking
-                    car.opticalFlowPoints = cv2.goodFeaturesToTrack(grayFrame, mask=fullImageMask, maxCorners=100, qualityLevel=0.3, minDistance=3, blockSize=7)
-        
-        if MASKRCNN_DRAW_MASKS:
-            maskRCNNDrawingMask
+                    # car.opticalFlowPoints = cv2.goodFeaturesToTrack(grayFrame, mask=fullImageMask, maxCorners=100, qualityLevel=0.3, minDistance=3, blockSize=7)
+
+                    # Register the car's bounding box with the multitracker
+                    multiTracker.add(cv2.legacy.TrackerKCF_create(), frame, [x, y, endX - x, endY - y])
 
         debugPrint(DETECTION_MODEL, "detection is complete")
         if MANUAL_HOMOGRAPHY == False:
@@ -503,7 +511,6 @@ while True:
             debugPrint("Homographies calculated")
 
         # Restart the timer.
-        currentTime = time.time()
         progress = 0
 
     # Try to track each car through the video with optical flow
@@ -511,67 +518,32 @@ while True:
 
     debugPrint("Tracking...")
     if not (lastGrayFrame is None):
-        for car in currentCars:
-            if car.opticalFlowPoints is not None and len(car.opticalFlowPoints) > 0:
-                oldFlowPoints = car.opticalFlowPoints
-                newFlowPoints, status, err = cv2.calcOpticalFlowPyrLK(lastGrayFrame, grayFrame, oldFlowPoints, None, winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        success, boxes = multiTracker.update(frame)
+
+        for i in range(len(currentCars)):
+            car = currentCars[i]
+            box = boxes[i]
+            #print([car.boundingBox.getX(),car.boundingBox.getY(),car.boundingBox.getWidth(),car.boundingBox.getHeight()],"vs",box)
             
+            oldBoundingBox = car.boundingBox
+            car.boundingBox = BoundingBox(box[0], box[1], box[2], box[3])
+            if car.boundingBox.getArea() != 0 and oldBoundingBox is not None:
                 homography = manualHomographyMatrix if MANUAL_HOMOGRAPHY else car.homographyMatrix
 
-                dx = 0
-                dy = 0
-                if newFlowPoints is not None:
-                    # We don't want to keep points that got lost
-                    previousLen = len(newFlowPoints)
-                    newFlowPoints = newFlowPoints[status == 1].reshape(-1,1,2)
-                    if len(newFlowPoints) != previousLen:
-                        debugPrint("Lost",previousLen - len(newFlowPoints),"optical flow points")
-                        if len(newFlowPoints) == 0:
-                            debugPrint("Car completely lost")
-                            continue
+                # Estimate the car's speed based on the movement of
+                # the bounding box in the top-down image
+                oldX, oldY = findPointInWarpedImage((oldBoundingBox.getCenterX(), oldBoundingBox.getCenterY()), homography)
+                newX, newY = findPointInWarpedImage((car.boundingBox.getCenterX(), car.boundingBox.getCenterY()), homography)
+                dx = newX - oldX
+                dy = newY - oldY
 
-                    # Save all of our data points so that the next bit of code doesn't remove
-                    # points that may become useful next iteration
-                    car.opticalFlowPoints = newFlowPoints.reshape(-1,1,2)
+                totalMovementInPixels = math.sqrt((dx * dx) + (dy * dy))
 
-                    # Remove outlying data points from our set so that they don't negatively
-                    # contribute to our bounding box estimate or our speed estimate
-                    
-                    if BOUNDING_BOX_TRACKING_METHOD == "naive":
-                        minX = newFlowPoints[0][0][0]
-                        minY = newFlowPoints[0][0][1]
-                        maxX = newFlowPoints[0][0][0]
-                        maxY = newFlowPoints[0][0][1]
-                        for point in newFlowPoints:
-                            if point[0][0] < minX:
-                                minX = point[0][0]
-                            elif point[0][0] > maxX:
-                                maxX = point[0][0]
-                            if point[0][1] < minY:
-                                minY = point[0][1]
-                            elif point[0][1] > maxY:
-                                maxY = point[0][1]
-                        car.boundingBox = BoundingBox(minX,minY,maxX-minX,maxY-minY)
-
-                    # Estimate the car's speed based on the movement of
-                    # the optical flow points in the top-down image
-                    for i in range(len(newFlowPoints)):
-                        if status[i] == 0:
-                            continue
-                        oldFlowPoint = findPointInWarpedImage((oldFlowPoints[i][0][0], oldFlowPoints[i][0][1]), homography)
-                        newFlowPoint = findPointInWarpedImage((newFlowPoints[i][0][0], newFlowPoints[i][0][1]), homography)
-                        dx += newFlowPoint[0] - oldFlowPoint[0]
-                        dy += newFlowPoint[1] - oldFlowPoint[1]
-                    
-                    dx = dx / len(newFlowPoints)
-                    dy = dy / len(newFlowPoints)
-                    totalMovementInPixels = math.sqrt((dx * dx) + (dy * dy))
-
-                    if MANUAL_HOMOGRAPHY:
-                        metersPerSecond = (totalMovementInPixels * FPS) / manualHomography_pixelsToMeters
-                        car.KMH = (metersPerSecond * 60 * 60) / 1000
-                    else:
-                        error("Not yet implemented")
+                if MANUAL_HOMOGRAPHY:
+                    metersPerSecond = (totalMovementInPixels * FPS) / manualHomography_pixelsToMeters
+                    car.KMH = (metersPerSecond * 60 * 60) / 1000
+                else:
+                    error("Not yet implemented")
 
                     
     debugPrint("Tracking complete")
@@ -584,13 +556,6 @@ while True:
         #print(frame.shape,maskRCNNDrawingMask.shape)
         #print(frame.dtype,maskRCNNDrawingMask.dtype)
         frame = cv2.addWeighted(frame, 1 - MASKRCNN_DRAW_MASKS_INTENSITY, maskRCNNDrawingMask.astype("uint8"), MASKRCNN_DRAW_MASKS_INTENSITY, 0)
-
-    for car in currentCars:
-        if not (car.opticalFlowPoints is None) and DRAW_OPTICAL_FLOW_POINTS:
-            for point in car.opticalFlowPoints:
-                transformedPoint = (int(point[0][0]), int(point[0][1]))
-                cv2.circle(drawingLayer,transformedPoint,OPTICAL_FLOW_POINT_DRAWING_SIZE,(255,255,255),-1)
-                cv2.circle(drawingLayer,transformedPoint,OPTICAL_FLOW_POINT_DRAWING_SIZE-4,(0,0,0),-1)
 
     if MANUAL_HOMOGRAPHY and DRAW_MANUAL_HOMOGRAPHY:
         # Draw the bounding boxes for the cars on the warped image as well
@@ -639,8 +604,6 @@ while True:
 
     # Save the current gray frame for next iteration
     lastGrayFrame = grayFrame
-    # Prepare the timer for next iteration
-    endTime = currentTime
 
 cap.release()
 if SAVE_RESULT_AS_VIDEO:
