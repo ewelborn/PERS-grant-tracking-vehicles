cc0e367facb92216e0d93d738bec9a37fa0517ed
Ethan Welborn <77039258+ewelborn@users.noreply.github.com>
2022-07-13 14:32:51 -0500

    Added image sequence to video script
    Just a small script to help convert image sequences to videos that can be read more easily by the main script.

---
+++ Scripts/imageSequenceToVideo.py
@@ -0,0 +1,31 @@
+# Script for importing an image sequence and exporting it as a video file
+import cv2
+
+imageSequencePath = "thermal_13Aug2020\\"
+imageNameTemplate = "frame_1_{i}.jpg"
+startIndex = 0 # Inclusive
+endIndex = 3528 # Inclusive
+
+outputVideoPath = "thermalSequence.mp4"
+outputFPS = 54
+
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = None
+
+for i in range(startIndex, endIndex + 1):
+    if i % 100 == 0:
+        print(i,"/",endIndex - startIndex + 1,"frames left")
+
+    frame = cv2.imread(imageSequencePath + imageNameTemplate.format(i=i))
+
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    if out is None:
+        out = cv2.VideoWriter(outputVideoPath, fourcc, outputFPS, (frameWidth, frameHeight))
+
+    out.write(frame)
+
+print("finished encoding")
+
+out.release()
